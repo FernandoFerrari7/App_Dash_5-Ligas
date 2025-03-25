969a06ed7e732a6f743abc92e3b9c6087963fed3
FernandoFerrari7 <fernandoaferrari@gmail.com>
2025-03-26 00:13:58 +0100

    Actualizacion gitignore

---
+++ config.py
@@ -0,0 +1,19 @@
+# Configuración de la aplicación
+
+# Configuración general
+APP_NAME = "Stats Dashboard 2024-2025"
+DEBUG = True
+
+# Configuración de seguridad
+SECRET_KEY = 'mi_secreto_super_seguro'  # Cambiar en producción a una clave segura
+
+# Configuración de autenticación
+ADMIN_USER = "admin"
+ADMIN_PASSWORD = "admin"
+SESSION_TIMEOUT = 600  # Tiempo de sesión en segundos (1 hora)
+
+# Configuración de rutas protegidas
+PROTECTED_ROUTES = [
+    '/dashboard-performance',
+    '/dashboard-medicina'
+]
